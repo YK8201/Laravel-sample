073a4cd00e8e8c29ba1a5936628a260a62f7e10f
YK8201 <kishiy2006@gmail.com>
2025-10-14 13:18:16 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>うわあああああああああああああああ!!!!!!!!!!!</h1>
+    </body>
+</html>
+
+
